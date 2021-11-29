0be42fb2b7851aae36ea425b808341bb28998d57
tzc-xuhao <xuhao19700101@163.com>
2021-11-29 19:58:42 +0800

    third-commit

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -7,5 +7,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello xuhao");
+        System.out.println("hot-fix");
     }
 }
